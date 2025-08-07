19fc1d9b198ad04bff1fb7bd7f12b507ecec069a
mamadou8bah <mbah18791@gmail.com>
2025-08-07 20:13:52 +0000

    Defined the structure for employee card

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,3 +1,4 @@
+import '../Styles/EmployeeCard.css';
 function EmployeeCard({ employee }) {
 
     const skills=employee.skills;
@@ -7,8 +8,14 @@ function EmployeeCard({ employee }) {
             <img src={employee.profilePic} alt={employee.firstName} />
             <div className="name-profession">
                 <h4>{employee.firstName+" "+employee.lastName}</h4>
-                <p>{employee.profession}</p>
+                <div className="profession-loca">
+                    <p>{employee.profession}</p>
+                <div className="location">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <span>{employee.location}</span>
+               </div>
             </div>
+        </div>
         </div>
         <div className="skills">
                 <ul>
@@ -17,7 +24,16 @@ function EmployeeCard({ employee }) {
                     ))}
                 </ul>
         </div>
-      
+        <div className="bio">
+            <p>{employee.bio}</p>
+        </div>
+        <div className="see-details">
+            <button className="details-btn">
+                <i className="fa-solid fa-circle-info"></i>
+                <span>See Details</span>
+            </button>
+        </div>
+        
     </div>
   );
 }
