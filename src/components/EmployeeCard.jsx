c26bb0e188216274330475ce6e4d3a8498bb0ea9
mamadou8bah <mbah18791@gmail.com>
2025-08-07 20:07:39 +0000

    Implementing Employee Card

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,6 +1,22 @@
 function EmployeeCard({ employee }) {
+
+    const skills=employee.skills;
   return (
     <div className="employee-card">
+        <div className="profile">
+            <img src={employee.profilePic} alt={employee.firstName} />
+            <div className="name-profession">
+                <h4>{employee.firstName+" "+employee.lastName}</h4>
+                <p>{employee.profession}</p>
+            </div>
+        </div>
+        <div className="skills">
+                <ul>
+                    {skills.slice(0,4).map((skill, index) => (
+                        <li key={index} className="skill-item">{skill}</li>
+                    ))}
+                </ul>
+        </div>
       
     </div>
   );
